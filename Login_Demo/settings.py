4b7f77e6464f30d392bfece7e9dc9af939a834aa
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-16 16:43:29 +0530

    secured secret key and updated requirements.txt with dependencies

--- Login_Demo/settings.py
+++ Login_Demo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_g+&e#sh+=1&l4d$9r=4ord!x$gm0%!0c)u6a(_*&4^u2%4x3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
