98d79f5afaea15789d61de63d77d8878f1ace862
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-28 17:00:28 +0530

    project got updated

--- Login_Demo/settings.py
+++ Login_Demo/settings.py
@@ -75,16 +75,13 @@ WSGI_APPLICATION = 'Login_Demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+# settings.py
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Or wherever your login view is
