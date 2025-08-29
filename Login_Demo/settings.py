d9b6160978dfab422c7261654bc15c3793319826
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-29 13:43:10 +0530

    updated project with static file handling and WhiteNoise integration

--- Login_Demo/settings.py
+++ Login_Demo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 LOGIN_URL = '/login/'
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static" ,
