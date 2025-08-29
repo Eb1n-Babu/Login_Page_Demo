93df626bd28b107906ff484a45497fe7f737d39e
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-29 12:00:12 +0530

    updated....

--- Login_Demo/settings.py
+++ Login_Demo/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "entry" / "static" / "css",
+    BASE_DIR / "static" ,
 ]
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
